b65e9a278132060800b0ac063570e0c69fdce2c2
alanhds <alan.souza@live.com>
2020-06-01 21:12:21 -0300

    criando objetos

--- 2Bimestre/scripts/todo.js
+++ 2Bimestre/scripts/todo.js
@@ -9,14 +9,47 @@
 var txtTarefa  = document.querySelector('#txtTarefa');
 var btnAdicionar  = document.querySelector('#btnAdicionar');
 var spnMensagem = document.querySelector('#spnMensagem');
+var tblTarefas = document.querySelector('#tblTarefas tbody');
 
+//objeto com mais de uma propriedade
+var objeto={
+    
+};
+
+var tarefas = [];//criando uma variavel tipo array
+
+
+console.log(tarefas);
 
 btnAdicionar.onclick = function(){
-    if( txtTarefa.value.trim() == ''){
-        spnMensagem.value = document.createTextNode("Por Favor preencha o campo!");
-       // alert("Por Favor preencha o campo!");
+    
+    spnMensagem.innerHTML = '';//limpa o valor dentro do html
+
+    var tarefa = txtTarefa.value.trim();//pega o texto e tira os espaços inuteis
+
+    if( tarefa == ''){
+      var textMensagem = document.createTextNode('Por favor, preencha o campo!');//cria o texto
+      spnMensagem.appendChild(textMensagem);
     }else{
 
+        //criando elementos da tabela
+            var linha = document.createElement('tr');
+            var celulaCodigo = document.createElement('td');
+            var celulaConteudo = document.createElement('td');  
+            var celulaRemover = document.createElement('td');
+        
+        //adicionando conteudo dentro da
+            celulaConteudo.appendChild(document.createTextNode(tarefa));
+
+        //adicionando celulas na linha
+            linha.appendChild(celulaCodigo);
+            linha.appendChild(celulaConteudo);
+            linha.appendChild(celulaRemover);
+
+        //adicionando a linha na tabela
+            tblTarefas.appendChild(linha);
+        
     }
 };
 
+ 
